026afed93ce2e69673fec56917fad3c2f4513f95
yukong <92148749+kyukong@users.noreply.github.com>
2023-02-28 18:03:55 +0900

    feat: 예외 처리 추가 (#18)
    * feat: JWT 예외 처리 세분화
    
    * feat: OAuth 예외 처리 추가
    
    * refactor: S3 예외 메시지 작성

--- src/main/java/com/dobugs/yologaauthenticationapi/exception/ControllerAdvice.java
+++ src/main/java/com/dobugs/yologaauthenticationapi/exception/ControllerAdvice.java
@@ -5,13 +5,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.dobugs.yologaauthenticationapi.exception.dto.response.ExceptionResponse;
+import com.dobugs.yologaauthenticationapi.support.exception.OAuthException;
 
 import io.awspring.cloud.s3.S3Exception;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import software.amazon.awssdk.awscore.exception.AwsServiceException;
 import software.amazon.awssdk.core.exception.SdkClientException;
 
 @RestControllerAdvice
 public class ControllerAdvice {
+
+    private static final int UNAUTHORIZED = 401;
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleInternalServerError(Exception e) {
@@ -25,15 +32,49 @@ public class ControllerAdvice {
         return ResponseEntity.badRequest().body(response);
     }
 
+    @ExceptionHandler(MalformedJwtException.class)
+    public ResponseEntity<ExceptionResponse> handleMalformedJWT(MalformedJwtException e) {
+        final ExceptionResponse response = ExceptionResponse.from("잘못된 형식의 JWT 입니다.");
+        return ResponseEntity.status(UNAUTHORIZED).body(response);
+    }
+
+    @ExceptionHandler(UnsupportedJwtException.class)
+    public ResponseEntity<ExceptionResponse> handleUnsupportedJWT(UnsupportedJwtException e) {
+        final String message = "지원하지 않는 JWT 입니다.";
+        final ExceptionResponse response = ExceptionResponse.from(message, e.getMessage());
+        return ResponseEntity.status(UNAUTHORIZED).body(response);
+    }
+
+    @ExceptionHandler(SignatureException.class)
+    public ResponseEntity<ExceptionResponse> handleSignatureException(SignatureException e) {
+        final ExceptionResponse response = ExceptionResponse.from("두벅스의 JWT 가 아닙니다.");
+        return ResponseEntity.status(UNAUTHORIZED).body(response);
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ExceptionResponse> handleExpiredJwtException(ExpiredJwtException e) {
+        final String message = "토큰의 만료 시간이 지났습니다.";
+        final ExceptionResponse response = ExceptionResponse.from(message, e.getMessage());
+        return ResponseEntity.status(UNAUTHORIZED).body(response);
+    }
+
+    @ExceptionHandler(OAuthException.class)
+    public ResponseEntity<ExceptionResponse> handleOAuthException(OAuthException e) {
+        final ExceptionResponse response = ExceptionResponse.from(e.getMessage());
+        return ResponseEntity.internalServerError().body(response);
+    }
+
     @ExceptionHandler({AwsServiceException.class, S3Exception.class})
     public ResponseEntity<ExceptionResponse> handleSdkServiceException(AwsServiceException e) {
-        final ExceptionResponse response = ExceptionResponse.from(e.getMessage());
+        final String message = "S3 에 연결하기 위한 과정에서 문제가 발생하였습니다.";
+        final ExceptionResponse response = ExceptionResponse.from(message, e.getMessage());
         return ResponseEntity.internalServerError().body(response);
     }
 
     @ExceptionHandler(SdkClientException.class)
     public ResponseEntity<ExceptionResponse> handleSdkClientException(SdkClientException e) {
-        final ExceptionResponse response = ExceptionResponse.from(e.getMessage());
+        final String message = "S3 와의 연결에서 문제가 발생하였습니다.";
+        final ExceptionResponse response = ExceptionResponse.from(message, e.getMessage());
         return ResponseEntity.internalServerError().body(response);
     }
 }
