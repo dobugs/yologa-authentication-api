9a681bc967ea3e5d193936693f21ac4015e7bd9c
yukong <kyb062666@gmail.com>
2023-03-12 00:05:46 +0900

    feat: yologa 접근 CORS 설정

--- src/main/java/com/dobugs/yologaauthenticationapi/config/WebConfiguration.java
+++ src/main/java/com/dobugs/yologaauthenticationapi/config/WebConfiguration.java
@@ -1,5 +1,6 @@
 package com.dobugs.yologaauthenticationapi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
+    @Value("${cors.auth.path}")
+    private String path;
+
+    @Value("${cors.auth.yologa.origin}")
+    private String yologa;
+
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .maxAge(3000);
+        registry.addMapping(path)
+            .allowedOrigins(yologa)
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .maxAge(3000);
     }
