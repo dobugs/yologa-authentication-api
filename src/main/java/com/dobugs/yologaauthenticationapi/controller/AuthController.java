8c3e5840fe6c3f7398c82549fed26fc7fc53ea8a
yukong <kyb062666@gmail.com>
2023-03-11 18:46:24 +0900

    feat: auth 검증 API 기능 구현

---
+++ src/main/java/com/dobugs/yologaauthenticationapi/controller/AuthController.java
@@ -0,0 +1,22 @@
+package com.dobugs.yologaauthenticationapi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dobugs.yologaauthenticationapi.config.auth.Authorized;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+@RestController
+public class AuthController {
+
+    @Authorized
+    @PostMapping("/login")
+    public ResponseEntity<Void> login() {
+        return ResponseEntity.ok().build();
+    }
+}
